34f33a598054278ac7dc31389bd50931d73050cd
Adam Clark <elm.majidi@gmail.com>
2020-11-13 22:22:04 +0100

    sets the proper headers for django server and also sets interceptors for 403 and requests a csrftoken when a 403 is raised

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getCookie } from '../util/Cookies';
 
 // window origin - base url
 const API_ENDPOINT = `${window.location.origin}/api`;
@@ -9,5 +10,50 @@ const api = axios.create({
     xsrfHeaderName: 'X-CSRFTOKEN',
     withCredentials: true
 });
+
+// Use interceptor to inject the token to requests
+api.interceptors.request.use(request => {
+    request.headers['X-CSRFTOKEN'] = getCookie('csrftoken');
+    return request;
+});
+
+api.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    // Return any error which is not due to authentication back to the calling service
+    if (error.response.status !== 403) {
+      return new Promise((resolve, reject) => {
+        reject(error);
+      });
+    }
+    // Logout user if token refresh didn't work or user is disabled
+    if (error.config.url === '/security/get-csrf/') {
+      return new Promise((resolve, reject) => {
+        reject(error);
+      });
+    }
+    // Try request again with new token
+    return api.get('/security/get-csrf/')
+      .then((response) => {
+
+        // New request with new token
+        const config = error.config;
+        config.headers['X-CSRFTOKEN'] = response.data.csrftoken;
+
+        return new Promise((resolve, reject) => {
+          api.request(config).then(response => {
+            resolve(response);
+          }).catch((error) => {
+            reject(error);
+          })
+        });
+
+      })
+      .catch((error) => {
+      	Promise.reject(error);
+      });
+}
+);
+
 
 export default api;
