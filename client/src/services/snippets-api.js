d0f615fce9fc1938af5fe3467b2ef1acfb58b7e4
Adam Clark <elm.majidi@gmail.com>
2020-11-13 22:23:15 +0100

    added trailing slashes in front of urls for django

--- client/src/services/snippets-api.js
+++ client/src/services/snippets-api.js
@@ -1,9 +1,9 @@
 import api from './api';
 
-export const fetchSnippetsService = () => api.get('/snippets');
+export const fetchSnippetsService = () => api.get('/snippets/');
 export const fetchPaginatedSnippetsService = page => api.get(`/snippets/?page=${page}`);
-export const fetchSnippetService = id => api.get(`/snippets/${id}`);
-export const createSnippetService = formData => api.post(`/snippets`, {...formData});
-export const updateSnippetService = formData => api.put(`/snippets/${formData.id}`, {...formData});
-export const deleteSnippetService = id => api.delete(`/snippets/${id}`);
+export const fetchSnippetService = id => api.get(`/snippets/${id}/`);
+export const createSnippetService = formData => api.post(`/snippets/`, {...formData});
+export const updateSnippetService = formData => api.put(`/snippets/${formData.id}/`, {...formData});
+export const deleteSnippetService = id => api.delete(`/snippets/${id}/`);
 
