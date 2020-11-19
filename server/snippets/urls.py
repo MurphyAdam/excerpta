3fa3d9f2835ac5784c2c97f5fea6868841495364
Adam Clark <elm.majidi@gmail.com>
2020-11-19 20:25:15 +0100

    removed no longer needed code

--- server/snippets/urls.py
+++ server/snippets/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from snippets.views import SnippetViewSet, api_root
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework.renderers import StaticHTMLRenderer
 
 snippet_list = SnippetViewSet.as_view({
 		'get': 'list',
@@ -15,15 +14,10 @@ snippet_detail = SnippetViewSet.as_view({
 		'delete': 'destroy'
 	})
 
-snippet_highlight = SnippetViewSet.as_view({
-		'get': 'highlight'
-	}, renderer_classes=[StaticHTMLRenderer])
-
 urlpatterns = [
     path('', api_root),
     path('snippets/', snippet_list, name='snippet-list'),
     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
