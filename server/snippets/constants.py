fd5a370c6b0ae70d0eb8aa7b086c436f825995b8
Adam Clark <elm.majidi@gmail.com>
2020-11-21 23:13:40 +0100

    a file for constants for the app

---
+++ server/snippets/constants.py
@@ -0,0 +1,38 @@
+# LANGUAGE_CHOICES for Snippet model 
+javascript = 'javascript'
+python = 'python'
+ruby = 'ruby'
+java = 'java'
+markdown = 'markdown'
+xml = 'xml'
+mysql = 'mysql'
+json = 'json'
+html = 'html'
+golang = 'golang'
+csharp = 'csharp'
+typescript = 'typescript'
+css = 'css'
+assembly_x86 = 'assembly_x86'
+c_cpp = 'c_cpp'
+jsx = 'jsx'
+
+LANGUAGE_CHOICES = sorted(
+		[
+			(javascript, javascript), 
+			(python, python), 
+			(ruby, ruby), 
+			(java, java),
+			(markdown, markdown),
+			(xml, xml), 
+			(mysql, mysql), 
+			(json, json), 
+			(html, html), 
+			(golang, golang), 
+			(csharp, csharp), 
+			(typescript, typescript), 
+			(css, css), 
+			(assembly_x86, assembly_x86),
+			(c_cpp, c_cpp),
+			(jsx, jsx)
+		]
+	)
