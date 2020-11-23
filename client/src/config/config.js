69b2ba1f92ef0a98915db053a2ee0e0d85066264
Adam Clark <elm.majidi@gmail.com>
2020-11-23 18:27:30 +0100

    removed moment.js and its configs

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,22 +1,3 @@
-import moment from 'moment';
-
-moment.locale('en', {
-    relativeTime : {
-        future: "in %s",
-        past:   "%s",
-        s:  "s",
-        m:  "1m",
-        mm: "%dm",
-        h:  "1h",
-        hh: "%dh",
-        d:  "1d",
-        dd: "%dd",
-        M:  "1mth",
-        MM: "%dmth",
-        y:  "1y",
-        yy: "%dy"
-    }
-});
 
 const config = {
   defaultPaletteColors: {
