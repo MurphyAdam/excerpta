1d5df27b39db1d9c52f7a5084bbb2165c9f190d6
Adam Clark <elm.majidi@gmail.com>
2020-11-27 15:01:52 +0100

    minor changes

--- client/src/services/snippets-api.js
+++ client/src/services/snippets-api.js
@@ -1,10 +1,10 @@
 import api from './api';
 
-export const fetchSnippetsService = () => api.get('/snippets/owned/');
+export const fetchSnippetsService = page => api.get(`/snippets/owned/?page=${page}`);
 export const fetchPaginatedSnippetsService = page => api.get(`/snippets/?page=${page}`);
 export const fetchSnippetService = id => api.get(`/snippets/owned/${id}/`);
 export const createSnippetService = formData => api.post(`/snippets/owned/`, {...formData});
 export const updateSnippetService = formData => api.put(`/snippets/owned/${formData.id}/`, {...formData});
 export const deleteSnippetService = id => api.delete(`/snippets/owned/${id}/`);
 
-export const fetchPublicSnippetsService = () => api.get('/snippets/');
+export const fetchPublicSnippetsService = page => api.get(`/snippets/?page=${page}`);
