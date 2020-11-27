3afc3072c4331e6c3618eddce0856f6de601d409
Adam Clark <elm.majidi@gmail.com>
2020-11-27 14:47:58 +0100

    added custom pagination class: CustomPageNumberPagination

--- server/projectx/settings.py
+++ server/projectx/settings.py
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'snippets.pagination.CustomPageNumberPagination',
     'PAGE_SIZE': 10,
     'DATE_FORMAT': '%B %d, %Y'
 }
