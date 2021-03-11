5827476a3e59aa2e9bfe23b01626e23e89b968da
Adam Clark <elm.majidi@gmail.com>
2021-03-11 18:52:04 +0100

    fixed bugs, and added token auth instead of session auth

--- server/security/views.py
+++ server/security/views.py
@@ -1,25 +1,31 @@
 from django.views.decorators.csrf import ensure_csrf_cookie, get_token
 from django.http import JsonResponse
 from rest_framework.views import APIView
-from rest_framework.authentication import SessionAuthentication
+from rest_framework import authentication, serializers
 from rest_framework.response import Response
-from users.serializers import UserSerializer
+from users.serializers import TokenSerializer
+
 
 @ensure_csrf_cookie
 def get_csrf_token(request):
-	"""
-	This will be `/api/set-csrf-cookie/` on `urls.py`
-	"""
-	token = get_token(request)
-	return JsonResponse({"csrftoken": token})
+    """
+        This will be `/api/set-csrf-cookie/` on `urls.py`
+        """
+    token = get_token(request)
+    return JsonResponse({"csrftoken": token})
 
 
 class CheckAuth(APIView):
-	authentication_classes = [SessionAuthentication]
+    authentication_classes = [authentication.TokenAuthentication]
 
-	def get(self, request, format=None):
-		user = UserSerializer(request.user, context={'request': request})
-		return Response({
-			'message': 'You are authenticated', 
-			'user': user.data
-			})
+    def get(self, request, format=None):
+        if request.auth:
+            serializer = TokenSerializer(request.auth,
+                                         context={'request': request})
+            return Response(serializer.data, 200)
+        return Response(
+            {
+                'message': 'You are not authenticated',
+                'user': None,
+                'key': None
+            }, 400)
